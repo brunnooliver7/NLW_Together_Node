f71392c6be97eaca185c51a6dec377c7df4f10c3
Bruno Oliveira <brunno.oliver7@gmail.com>
2022-06-11 20:18:23 -0300

    Dia 5 - middleware ensureAuthenticated

---
+++ src/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,38 @@
+import { Request, Response, NextFunction } from "express";
+import { verify } from "jsonwebtoken";
+
+interface IPayload {
+  sub: string;
+}
+
+export function ensureAuthenticated(
+  request: Request,
+  response: Response,
+  next: NextFunction
+) {
+  // Receber o token
+  const authToken = request.headers.authorization;
+
+  // Validar se token está preenchido
+  if (!authToken) {
+    return response.status(401).end();
+  }
+
+  // Ignorar a palavra 'Bearer'
+  const [, token] = authToken.split(" ");
+
+  try {
+    // Validar se token é válido
+    const { sub } = verify(
+      token,
+      "669cc0a136d08138f12f565c2194272c"
+    ) as IPayload;
+
+    // Recuperar informações do usuário
+    request.user_id = sub;
+
+    return next();
+  } catch (err) {
+    return response.status(401).end();
+  }
+}
